e664cd06ec3c4835f823af377a19bc3be255db4b
OrghoN <anoronyo@gmail.com>
2018-07-17 06:44:15 -0400

    figured out rotation and translation

--- wireMatrixGenerator.cpp
+++ wireMatrixGenerator.cpp
@@ -1,14 +1,22 @@
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/aff_transformation_tags.h>
 #include <CGAL/Boolean_set_operations_2.h>
+#include <CGAL/number_utils.h>
 #include <list>
+#include <cmath>
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
-typedef Kernel::Point_2 Point_2;
+typedef CGAL::Aff_transformation_2<Kernel>              Transformation;
 typedef CGAL::Polygon_2<Kernel>                           Polygon_2;
 typedef CGAL::Polygon_with_holes_2<Kernel>                Polygon_with_holes_2;
+typedef Kernel::Point_2 Point_2;
 typedef std::list<Polygon_with_holes_2>                   Pwh_list_2;
+typedef CGAL::Vector_2<Kernel> Vector;
+typedef CGAL::Direction_2<Kernel> Direction;
+CGAL_EXPORT extern const CGAL::Translation              TRANSLATION;
 
 #include "print_utils.h"
+
 
 Polygon_with_holes_2 intersection(Polygon_2 &polygon1, Polygon_2 &polygon2)
 {
@@ -24,9 +32,26 @@ Polygon_with_holes_2 intersection(Polygon_2 &polygon1, Polygon_2 &polygon2)
         return outputPolygon;
 }
 
+Polygon_with_holes_2 intersection(Polygon_with_holes_2 &polygon1, Polygon_2 &polygon2)
+{
+        Pwh_list_2 result;
+        Polygon_with_holes_2 outputPolygon;
+
+        CGAL::intersection (polygon1, polygon2, std::back_inserter(result) );
+
+        for(auto it = result.begin(); it != result.end(); it++) {
+                outputPolygon = *it;
+        }
+
+        return outputPolygon;
+}
+
+
 int main ()
 {
-        // Transformation rotate(ROTATION, sin(pi/2), cos(pi/2));
+        double angle = M_PI/3;
+        Transformation rotate(cos(angle), -sin(angle), sin(angle), cos(angle), 1);
+        Transformation translate(TRANSLATION, Vector(-2, 0));
 
         Polygon_2 w0;
         w0.push_back (Point_2 (1,-10));
@@ -36,20 +61,21 @@ int main ()
         std::cout << "w0 = "; print_polygon (w0);
 
         Polygon_2 v2;
-        v2.push_back (Point_2 (10,-1));
-        v2.push_back (Point_2 (10,1));
-        v2.push_back (Point_2 (-10,1));
-        v2.push_back (Point_2 (-10,-1));
+        v2.push_back (translate(Point_2 (10,-1)));
+        v2.push_back (translate(Point_2 (10,1)));
+        v2.push_back (translate(Point_2 (-10,1)));
+        v2.push_back (translate(Point_2 (-10,-1)));
         std::cout << "v2 = "; print_polygon (v2);
 
         Polygon_2 u0;
-        u0.push_back (Point_2 (-3,-1000));
-        u0.push_back (Point_2 (-3,1000));
-        u0.push_back (Point_2 (-1,1000));
-        u0.push_back (Point_2 (-1,1000));
+        u0.push_back (rotate(Point_2 (1,-10)));
+        u0.push_back (rotate(Point_2 (1,10)));
+        u0.push_back (rotate(Point_2 (-1,10)));
+        u0.push_back (rotate(Point_2 (-1,-10)));
         std::cout << "u0 = "; print_polygon (u0);
 
-        print_polygon_with_holes(intersection(w0, v2));
+        Polygon_with_holes_2 intersect = intersection(w0, v2);
+        print_polygon_with_holes(intersection(intersect, u0));
 
         return 0;
         // if ((CGAL::do_intersect(w0, v2)))
